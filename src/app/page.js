0065b7769e7645c1d9e895fed48be7cb7f6f2acf
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2023-10-12 02:18:51 -0300

    config module

--- src/app/page.js
+++ src/app/page.js
@@ -2,16 +2,17 @@
 
 import { Scoreboard } from './components/ScoreBoard'
 import Particles from './particles'
-
+import Image from 'next/image'
+import '@/../public/logo-bap-removebg.png'
 
 export default function Home() {
 
   const logo = 'https://github.com/solidsnk86/tablero-369/blob/master/public/logo-bap-removebg.png?raw=true'
   return (
     <main className="flex relative min-h-screen justify-center m-auto">
-      <figure className='logo'>
-          <img src={logo} width={140} height={140} />
-        </figure>
+              <figure className='logo'>
+                <Image src={logo} width={140} height={140} />
+              </figure>
         <Particles className='absolute inset-0 -z-10 animate-fade-in' quantity={133} />
         <Scoreboard />
     </main>
